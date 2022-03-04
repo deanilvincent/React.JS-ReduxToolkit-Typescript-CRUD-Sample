de52919ac7ef40213fc51a7f5059cb9c9e781053
Mark Vicente <mark@anynetworksystems.com>
2022-03-04 11:26:03 +0800

    add process.env

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+const development = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
 let baseURL = "https://localhost:7128/api"
+if (!development)
+    baseURL = "https://basicemployeedirectorywebapi.azurewebsites.net/api"
+
 
 export default axios.create({
     baseURL
